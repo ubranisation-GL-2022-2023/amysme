4220f1fd153220d8bed3fa0a8ecf194d319acb07
Ramzi-dev611 <ramzi.latrous@insat.ucar.tn>
2022-11-27 02:37:11 +0100

    enhancement: set up TypeORM and configModule

--- webportal-back/src/app.module.ts
+++ webportal-back/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseConfigService } from './database-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      envFilePath: 'local.env',
+     }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+      inject: [DatabaseConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseConfigService],
 })
 export class AppModule {}
