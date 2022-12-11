30e00b77af089b274dc682a361effdb6b7ce5478
Ramzi-dev611 <ramzi.latrous@insat.ucar.tn>
2022-12-11 04:52:18 +0100

    fix: first approach to solve the problem of the join query to list customer demands by user

--- webportal-back/src/client/services/customer-demand.service.ts
+++ webportal-back/src/client/services/customer-demand.service.ts
@@ -20,5 +20,8 @@ export class CustomerDemandService extends CrudService<CustomerDemandEntity> {
 
   public async findByUser(user: User): Promise<CustomerDemandEntity[]> {
     // need to read about query builder to create an inner join query
+    return await this.repo.query(
+      `select * from customerDemand where user_id = ${user.id}`,
+    );
   }
 }
