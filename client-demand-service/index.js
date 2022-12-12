540dbfb5065ea28af24253390215fd05ec769ae3
hamza <mahjoubhamza036@gmail.com>
2022-12-12 18:17:49 +0100

    added client demand service front and rabbit mq event

--- client-demand-service/index.js
+++ client-demand-service/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
+var http = require('http');
 
+const { Server } = require("socket.io");
+var { Subscriber } = require("./rabbitmq/subscriber");
+
+var { Subscriber } = require("./rabbitmq/subscriber");
 
  //   766$
 const One_Room_mid = [
@@ -231,11 +236,9 @@ app.get("/", (req, res) => {
   res.render("index", {test: 'hello'});
 });
 
-app.post("/demande", (req, res) => {
-  let data  = req.body
+const handleDemande = (data) => {
   let hardware = []
   let totalPrice = 0
-  
   if(Math.floor(data.max_budget / data.rooms) < 766) {
     for(let i = 0; i < data.rooms; i++) {
       hardware = hardware.concat(One_Room_low)
@@ -271,15 +274,68 @@ app.post("/demande", (req, res) => {
       }
     } 
   } 
+  return {
+    hardware,
+    totalPrice
+  }
+}
 
-  res.render("index", {
-    demandId: 1,
-    userId: data.userId,
-    houseData: hardware,
-    totalBudget: totalPrice
-  });
+app.post("/demand", (req, res) => {
+  let data  = req.body
+
+  if(data) {
+    const {hardware, totalPrice} = handleDemande(data);
+    res.json({
+      demandId: 1,
+      userId: data.userId,
+      houseData: hardware,
+      totalBudget: totalPrice
+    });
+  
+  } else
+  res.json({message:" No data provided "})
+
 })
 
-app.listen(3000, () => {
-  console.log("server started on port 3000");
+app.set('port',3002)
+
+var server = http.createServer(app);
+
+server.listen(3002)
+
+const io = new Server(server,{cors:{origin:"*"}});
+
+
+var subscriberOptions = {
+    exchange: "Documents_Exchange",
+    queueName: "generated_data_queue",
+    routingKeys: ["generated_data"],
+  };
+
+function onIncomingMessage(message) {
+    // console.log("message", message);
+    const newMessage = JSON.parse(message.content.toString())
+    subscriber.ack(message)
+    console.log(newMessage);
+    const {hardware, totalPrice} = handleDemande(newMessage);
+    io.emit('data',{
+      demandId: 2,
+      userId: newMessage.userId,
+      houseData: hardware,
+      totalBudget: totalPrice
+    })
+
+}
+
+io.on('connection', (socket) => {
+  let previousId;
+
+  const safeJoin = currentId => {
+    socket.leave(previousId);
+    socket.join(currentId, () => console.log(`Socket ${socket.id} joined room ${currentId}`));
+    previousId = currentId;
+  };
 });
+
+var subscriber = new Subscriber(subscriberOptions);
+subscriber.start(onIncomingMessage);
