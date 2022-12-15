1696c078e5a6658985140632e5a16e3d9b18cdff
Kanoun Zied <64085047+kanounzied@users.noreply.github.com>
2022-12-15 09:44:29 +0100

    fixed excel problem

--- file_transformation_service/excel_reader.py
+++ file_transformation_service/excel_reader.py
@@ -1,16 +1,15 @@
+import json
+
 import pandas as pd
 
 
 def readEXCEL(file_path):
   df = pd.read_excel(file_path)
   data = df.to_json(orient='records')
-  json = {
-    "data": data
-  }
-  return json
+  return data
 
 def json_to_excel(data):
-  if data["data"] != None:
-    pd.DataFrame(data["data"]).to_excel("out/generated_data_frame.xlsx")
-  else:
-    pd.DataFrame(data).to_excel("out/generated_data_frame.xlsx")
+  # if data["data"] != None:
+  #   pd.DataFrame(data["data"]).to_excel("out/generated_data_frame.xlsx")
+  # else:
+  pd.DataFrame(data).to_excel("out/generated_data_frame.xlsx")
--- file_transformation_service/transform_files_apis.py
+++ file_transformation_service/transform_files_apis.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 
 from csv_reader import readCSV
@@ -19,28 +21,41 @@ files_root = "files/"
 
 @app.get("/csv/<filename>")
 def transform_csv(filename):
-  return readCSV(files_root + filename)
-
+  if check_file(files_root + filename):
+    return readCSV(files_root + filename)
+  else:
+    return 'file not found'
 
 @app.get("/pdf/contract/<filename>")
 def transform_pdf_contract(filename):
-  return readPDF_contract(files_root + filename)
+  if check_file(files_root + filename):
+    return readPDF_contract(files_root + filename)
+  else:
+    return 'file not found'
 
 
 @app.get("/pdf/cv/<filename>")
 def transform_pdf_cv(filename):
-  return readPDF_cv(files_root + filename)
+  if check_file(files_root + filename):
+    return readPDF_cv(files_root + filename)
+  else:
+    return 'file not found'
 
 
 @app.get("/excel/<filename>")
 def transform_excel(filename):
-  return readEXCEL(files_root + filename)
+  if check_file(files_root + filename):
+    return readEXCEL(files_root + filename)
+  else:
+    return 'file not found'
 
 
 @app.get("/yaml/<filename>")
 def transform_yaml(filename):
-  return readYAML(files_root + filename)
-
+  if check_file(files_root + filename):
+    return readYAML(files_root + filename)
+  else:
+    return 'file not found'
 
 @app.post("/excel")
 def reverse_excel():
@@ -48,6 +63,8 @@ def reverse_excel():
   json_to_excel(data)
   return data
 
+def check_file(path):
+  return os.path.exists(path)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
